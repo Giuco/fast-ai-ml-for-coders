58d9f6cf9d7104f4f5d06b6304c73ca52286cb78
Giuco <giucoferrari@gmail.com>
2019-07-11 01:39:58 -0300

    Update

--- lessons/helpers.py
+++ lessons/helpers.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from pandas.api.types import is_datetime64_any_dtype
-from typing import Optional, List
+from pandas.api.types import is_datetime64_any_dtype, is_string_dtype
+from typing import Optional, List, Dict
 import re
 from toolz import compose
 
@@ -48,3 +48,19 @@ def add_date_parts(df: pd.DataFrame, columns: Optional[List[str]] = None, drop=F
         df = df.drop(columns, axis=1)
     
     return df
+
+
+def tranform_columns_to_categorical(df: pd.DataFrame, ordered: Dict = dict()) -> pd.DataFrame:
+    df = df.copy()
+    
+    for n,c in df.items():
+        if is_string_dtype(c):
+            df[n] = c.astype('category').cat.as_ordered()
+            
+            if n in ordered:
+                df[n] = df[n].cat.set_categories(ordered[n], ordered=True)
+
+    return df
+
+
+def get_mapper(df: pd.DataFrame, ignored_columns: Optional[List[śtr]], )
